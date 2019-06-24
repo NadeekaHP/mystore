2824c7f1b9feda98d3df7df998fbe4261cbb2ed2
Nadeeka Pathirana <32059848+NadeekaHP@users.noreply.github.com>
2019-06-24 19:48:56 +0530

    adding discounted price

--- src/store.js
+++ src/store.js
@@ -6,11 +6,11 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     forSale: [
-        { invId: 1, name: 'Ladies Bag', image: '/assets/1.jpg', price: 999 },
-        { invId: 2, name: 'Ladies Watch', image: '/assets/2.jpg', price: 1499 },
-        { invId: 3, name: 'Ladies Shoes', image: '/assets/3.jpg', price: 499 },
-        { invId: 4, name: 'Makeup', image: '/assets/4.jpg', price: 299 },
-      ],
+      { invId: 1, name: 'Ladies Bag', image: '/assets/1.jpg', price: 999, discounted: 1500, details:"" },
+      { invId: 2, name: 'Ladies Watch', image: '/assets/2.jpg', price: 1399, discounted: 2450 },
+      { invId: 3, name: 'Ladies Shoes', image: '/assets/3.jpg', price: 799, discounted: 880 },
+      { invId: 4, name: 'Makeup', image: '/assets/4.jpg', price: 599, discounted: 780 },
+    ],
       inCart: [],
   },
   getters: {
